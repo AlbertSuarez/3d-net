f51965a40fbc88fa5063fdcdda73d500682afe7e
Albert Suarez <alsumo95@gmail.com>
2020-11-28 13:41:33 +0100

    ✅ Added test for checking config

---
+++ test/test_config.py
@@ -0,0 +1,20 @@
+import unittest
+
+from src.config import DATASET_CATEGORIES
+
+
+class ConfigTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_categories(self):
+        self.assertEqual(len(DATASET_CATEGORIES.keys()), len(set(DATASET_CATEGORIES.keys())))
+        self.assertTrue(all(k.count('__') == 1 for k in DATASET_CATEGORIES.keys()))
+        self.assertTrue(all('category_id' in v for v in DATASET_CATEGORIES.values()))
+        self.assertTrue(all('main_category' in v for v in DATASET_CATEGORIES.values()))
+        self.assertTrue(all(k.startswith(f'{v.get("main_category")}__') for k, v in DATASET_CATEGORIES.items()))
+        self.assertEqual(
+            len([v.get('category_id') for v in DATASET_CATEGORIES.values()]),
+            len(set([v.get('category_id') for v in DATASET_CATEGORIES.values()]))
+        )
